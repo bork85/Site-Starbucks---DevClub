d68a483913e41387c1915e80831e85f445b4869e
Daniel Bork <daniel.bork@yahoo.com.br>
2025-10-27 11:12:21 -0300

    commit final

--- scrips.js
+++ scrips.js
@@ -1,10 +1,17 @@
-let circulo = document.querySelector(".circulo")
-let copo = document.querySelector(".copo")
+let circulo = document.querySelector(".circulo");
+let copo = document.querySelector(".copo");
+let button = document.querySelector(".button");
 
 function trocarCor(color) {
-    circulo.style.background = color
+  circulo.style.background = color;
 }
 function trocaCopo(img) {
-    copo.src = img
-    copo.style.width = '400px'
+  copo.src = img;
+  copo.style.width = "400px";
+}
+
+function clickButton() {
+  alert("Você será redirecionado para o meu portfólio! Obrigado!");
+
+  window.location.href = "https://portfolio-daniel-bork.vercel.app";
 }
